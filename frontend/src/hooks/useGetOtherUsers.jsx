a82111828897ac95e370697489fa62f7346c6489
Mahendra Chandel <mahendrachandel040@gmail.com>
2025-04-09 02:00:05 +0530

    Update useGetOtherUsers.jsx final

--- frontend/src/hooks/useGetOtherUsers.jsx
+++ frontend/src/hooks/useGetOtherUsers.jsx
@@ -10,7 +10,7 @@ const useGetOtherUsers = () => {
         const fetchOtherUsers = async () => {
             try {
                 axios.defaults.withCredentials = true;
-                const res = await axios.get(`https://chat-app-vkfw.onrender.com/api/v1/user`);
+                const res = await axios.get("https://chat-app-vkfw.onrender.com/api/v1/user");
                 // store
                 console.log("other users -> ",res);
                 dispatch(setOtherUsers(res.data));
